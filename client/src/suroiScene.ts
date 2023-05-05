09e6494a05404da44ac97047fd78966ee6aa5ff0
Henry Sanger <henry@sanger.io>
2023-05-05 08:46:40 -0400

    use default zoom

--- client/src/suroiScene.ts
+++ client/src/suroiScene.ts
@@ -27,28 +27,23 @@ export class SuroiScene extends Phaser.Scene {
                 const altFist: boolean = Math.random() < 0.5;
                 this.tweens.add({
                     targets: altFist ? this.playerLeftFist : this.playerRightFist,
-                    x: 25,
-                    y: altFist ? 8 : -8,
-                    duration: 75,
+                    x: 50,
+                    y: altFist ? 16 : -16,
+                    duration: 100,
                     yoyo: true,
                     onComplete: () => this.punching = false
                 });
                 this.sound.add("swing").play();
             }
         });
-        this.cameras.main.setZoom(2);
     }
 
     create(): void {
-        const playerBody = this.add.circle(0, 0, 16, 0xffdbac);
-        this.playerLeftFist = this.add.circle(12, 12, 5, 0xffdbac).setStrokeStyle(1.5, 0xb28953);
-        this.playerRightFist = this.add.circle(12, -12, 5, 0xffdbac).setStrokeStyle(1.5, 0xb28953);
-        this.add.grid(0, 0, 7200, 7200, 160, 160, 0x49993e);
-        //this.add.image(360, 360, "tree_01").setScale(.5, .5);
-        for(let x = 0; x < 720; x++) {
-            this.add.rectangle(x, 0, 720, 2, 0x000000);
-        }
-        this.player = this.add.container(360, 360, [playerBody, this.playerLeftFist, this.playerRightFist]);
+        const playerBody = this.add.circle(0, 0, 32, 0xffdbac);
+        this.playerLeftFist = this.add.circle(24, 24, 10, 0xffdbac).setStrokeStyle(3, 0x8e6b3d);
+        this.playerRightFist = this.add.circle(24, -24, 10, 0xffdbac).setStrokeStyle(3, 0x8e6b3d);
+        this.add.grid(0, 0, 14400, 14400, 640, 640, 0x49993e);
+        this.player = this.add.container(48, 48, [playerBody, this.playerLeftFist, this.playerRightFist]);
         this.cameras.main.startFollow(this.player);
     }
 
