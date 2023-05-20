9154bc83bce4809bd000af3bd5d95980fd762a5f
Damien Vesper <ldamienvesper@gmail.com>
2023-05-20 17:24:47 -0500

    chore: lint main

--- client/src/scripts/main.ts
+++ client/src/scripts/main.ts
@@ -20,28 +20,30 @@ $(() => {
 
     // Join server when play button is clicked
     const playSoloBtn: JQuery = $("#btn-play-solo");
-    const reEnablePlayButton = (): void => {
+
+    const enablePlayButton = (): void => {
         playSoloBtn.removeClass("btn-disabled");
         playSoloBtn.prop("disabled", false);
         playSoloBtn.text("Play Solo");
     };
+
     playSoloBtn.on("click", (): void => {
         playSoloBtn.addClass("btn-disabled");
         playSoloBtn.prop("disabled", true);
         playSoloBtn.text("Connecting...");
-        void $.get(`${API_URL}/getGame`, data => {
-            /* eslint-disable-next-line no-new,@typescript-eslint/restrict-template-expressions */
-            core.game?.connect(`${data.addr}?name=${$("#username-input").val()}`);
-            reEnablePlayButton();
+        void $.get(`${API_URL}/getGame`, (data: { addr: string }) => {
+            core.game?.connect(`${data.addr}?name=${$("#username-input").val() as string}`);
+            enablePlayButton();
         }).fail((): void => {
             $("#splash-server-message-text").text("Error finding game.");
             $("#splash-server-message").show();
-            reEnablePlayButton();
+            enablePlayButton();
         });
     });
 
     const usernameField: JQuery = $("#username-input");
     const nickname: string | null = localStorage.getItem("nickname");
+
     if (nickname !== null) {
         usernameField.val(nickname);
     }
