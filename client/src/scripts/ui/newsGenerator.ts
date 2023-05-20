2be7bd0a47ddd89fef96c911ac7236758304e48d
Damien Vesper <ldamienvesper@gmail.com>
2023-05-20 17:15:09 -0500

    chore (news): lint

--- client/src/scripts/ui/newsGenerator.ts
+++ client/src/scripts/ui/newsGenerator.ts
@@ -3,7 +3,7 @@
  * Fields:
  *  - date (required): The UNIX timestamp for Date.now() in milliseconds.
  *  - author (required): Whoever wrote the post. If you do not wish to list yourself, then simply label it as "Suroi Developers."
- *  - title (required):Title of the post.
+ *  - title (required): Title of the post.
  *  - bannerImage (optional): The URL of the banner image for the news post. It should have a 2:1 or greater aspect ratio.
  *  - content (required): Brief information about the news psot that appears on the homepage. Remember to use backslashes \ before any quotation marks! Supports HTML.
  *  - longContent (optional): Longer information for the news page. Defaults to the value of content. Supports HTML.
@@ -13,26 +13,29 @@ import { news } from "./newsPosts";
 
 let newsText = "";
 for (const newsPost of news) {
+    const date: string = new Date(newsPost.date).toLocaleDateString("default", {
+        month: "long",
+        day: "numeric",
+        year: "numeric"
+    });
+
     newsText += '<article class="splash-news-entry">';
     newsText += `<h3 class="news-title">${newsPost.title}</h3>`;
-    const date: string = new Date(newsPost.date)
-        .toLocaleDateString("default", {
-            month: "long",
-            day: "numeric",
-            year: "numeric"
-        });
     newsText += `<div class="news-date">${date}`;
     newsText += `<br><i>Written by: ${newsPost.author}</i>`;
     newsText += "</div>";
+
     if (newsPost.bannerImage !== undefined) {
         newsText += `<span class="news-banner"><img src="${newsPost.bannerImage}" alt="${newsPost.title}"></span><br>`;
     }
+
     if (newsPost.longContent !== undefined) {
         newsText += `<div class="short-desc">${newsPost.content}</div>`;
         newsText += `<div class="long-desc">${newsPost.longContent}</div>`;
     } else {
         newsText += `<div class="long-desc">${newsPost.content}</div>`;
     }
+
     newsText += "</article>";
 }
 
--- client/src/scripts/ui/newsWidget.ts
+++ client/src/scripts/ui/newsWidget.ts
@@ -2,13 +2,13 @@ import { news } from "./newsPosts";
 
 let newsText = "";
 for (const newsPost of news.slice(0, 5)) {
+    const date: string = new Date(newsPost.date).toLocaleDateString("default", {
+        month: "long",
+        day: "numeric",
+        year: "numeric"
+    });
+
     newsText += '<article class="splash-news-entry">';
-    const date: string = new Date(newsPost.date)
-        .toLocaleDateString("default", {
-            month: "long",
-            day: "numeric",
-            year: "numeric"
-        });
     newsText += `<div class="news-date">${date}</div>`;
     newsText += `<div class="news-title">${newsPost.title}</div>`;
     newsText += `<p>${newsPost.content}<br><i>- ${newsPost.author}</i></p></article>`;
