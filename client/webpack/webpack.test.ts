61c63f4dc8d265438e67aa33492081152bf1ddab
Damien Vesper <ldamienvesper@gmail.com>
2023-05-20 22:28:54 -0500

    feat: test configuration

---
+++ client/webpack/webpack.test.ts
@@ -0,0 +1,29 @@
+import merge from "webpack-merge";
+import common from "./webpack.common";
+
+import { DefinePlugin } from "webpack";
+import CopyPlugin from "copy-webpack-plugin";
+
+import * as path from "path";
+
+const config = merge(common, {
+    mode: "development",
+
+    output: {
+        path: path.resolve(__dirname, "../dist"),
+        filename: "js/[name].[chunkhash:8].js",
+        clean: true
+    },
+
+    plugins: [
+        new DefinePlugin({ API_URL: "\"/api\"" }),
+        new CopyPlugin({
+            patterns: [{
+                from: path.resolve(__dirname, "../public"),
+                to: path.resolve(__dirname, "../dist")
+            }]
+        })
+    ]
+});
+
+export default config;
