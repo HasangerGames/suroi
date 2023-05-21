28eacab1b10156f4a3526b009deb5a55e94be83b
Rohan Gupta <rohansgupta17@gmail.com>
2023-05-21 12:55:41 -0400

    >100 dmg bug fix, added heal crates

--- common/src/definitions/obstacles.ts
+++ common/src/definitions/obstacles.ts
@@ -117,6 +117,17 @@ export class Obstacles extends ObjectDefinitions {
             hitbox: new CircleHitbox(3.45),
             rotation: "full",
             explosion: "super_barrel_explosion"
-        }
+        },
+        {
+            idString: "crate_health",
+            material: "crate",
+            health: 150,
+            scale: {
+                spawnMin: 1.0, spawnMax: 1.0, destroy: 0.6
+            },
+            hitbox: new RectangleHitbox(v(-4.3, -4.3), v(4.3, 4.3)),
+            rotation: "limited",
+            explosion: "crate_health_explosion"
+        },
     ];
 }
