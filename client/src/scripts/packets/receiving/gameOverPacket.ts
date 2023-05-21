f872cdbcbe3ce36c9119aaed88aa41e43acbaf42
leo78913 <leo3758@riseup.net>
2023-05-21 12:15:13 -0300

    feat: add time alive to death screen

--- client/src/scripts/packets/receiving/gameOverPacket.ts
+++ client/src/scripts/packets/receiving/gameOverPacket.ts
@@ -1,6 +1,5 @@
 import { type Player } from "../../objects/player";
 import { ReceivingPacket } from "../../types/receivingPacket";
-
 import { type SuroiBitStream } from "../../../../../common/src/utils/suroiBitStream";
 
 export class GameOverPacket extends ReceivingPacket {
@@ -14,7 +13,11 @@ export class GameOverPacket extends ReceivingPacket {
         $("#game-over-damage-done").text(stream.readUint16()); // damage done
         $("#game-over-damage-taken").text(stream.readUint16()); // damage taken
 
-        // const timeAlive = new Date(stream.readUint16() * 1000); // time alive
+        const timeAlive = new Date(stream.readUint16() * 1000); // time alive
+        let timeString = "";
+        if (timeAlive.getMinutes() > 0) timeString += `${timeAlive.getMinutes()}m`;
+        timeString += `${timeAlive.getSeconds()}s`
+        $("#game-over-time").text(timeString);
 
         setTimeout(() => $("#game-over-screen").fadeIn(1000), 3000);
     }
