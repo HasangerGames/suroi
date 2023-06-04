f1605f7eed4fb376b035191d98860a7e82676868
Henry Sanger <henry@sanger.io>
2023-06-04 19:12:41 -0400

    enhance: buff gas a lot

--- server/src/data/gasStages.ts
+++ server/src/data/gasStages.ts
@@ -35,62 +35,62 @@ export const GasStages: GasStage[] = [
         duration: 30,
         oldRadius: 256,
         newRadius: 128,
-        dps: 1.5
+        dps: 2
     },
     {
         mode: GasMode.Advancing,
         duration: 15,
         oldRadius: 256,
         newRadius: 128,
-        dps: 2
+        dps: 3
     },
     {
         mode: GasMode.Waiting,
         duration: 20,
         oldRadius: 128,
         newRadius: 64,
-        dps: 2.5
+        dps: 4
     },
     {
         mode: GasMode.Advancing,
         duration: 10,
         oldRadius: 128,
         newRadius: 64,
-        dps: 3
+        dps: 5
     },
     {
         mode: GasMode.Waiting,
         duration: 15,
         oldRadius: 64,
         newRadius: 16,
-        dps: 3.5
+        dps: 6
     },
     {
         mode: GasMode.Advancing,
         duration: 5,
         oldRadius: 64,
         newRadius: 32,
-        dps: 4
+        dps: 7
     },
     {
         mode: GasMode.Waiting,
         duration: 10,
         oldRadius: 32,
         newRadius: 0,
-        dps: 4.5
+        dps: 8
     },
     {
         mode: GasMode.Advancing,
         duration: 5,
         oldRadius: 32,
         newRadius: 0,
-        dps: 5
+        dps: 9
     },
     {
         mode: GasMode.Waiting,
         duration: 0,
         oldRadius: 0,
         newRadius: 0,
-        dps: 5.5
+        dps: 10
     }
 ];
