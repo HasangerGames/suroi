535f8e87e34639901589e6feeaa320e7ea60994d
leo78913 <leo3758@riseup.net>
2023-06-27 17:20:04 -0300

    fix: fix player.dirty.inventory not being set to false

--- server/src/inventory/inventory.ts
+++ server/src/inventory/inventory.ts
@@ -365,6 +365,7 @@ export class Inventory {
 
         stream.writeBoolean(this.owner.dirty.inventory);
         if (this.owner.dirty.inventory) {
+            this.owner.dirty.inventory = false;
             for (const count of Object.values(this.owner.inventory.items)) {
                 stream.writeBoolean(count > 0); // Has item
                 if (count > 0) stream.writeUint8(count);
