39a939d3fbc0f9dba4e5895e2c8fc3b38315cd95
leo78913 <leo3758@riseup.net>
2023-07-01 14:28:35 -0300

    fix: fix not being able to empty the user name input

--- client/src/scripts/ui.ts
+++ client/src/scripts/ui.ts
@@ -39,11 +39,7 @@ $((): void => {
 
     usernameField.val(localStorageInstance.config.playerName);
     usernameField.on("input", () => {
-        const value = usernameField.val() as string | undefined;
-
-        if (value !== undefined && value.trim().length > 0) {
-            localStorageInstance.update({ playerName: value });
-        }
+        localStorageInstance.update({ playerName: usernameField.val() as string });
     });
 
     const serverSelect: JQuery<HTMLSelectElement> = $("#server-select");
