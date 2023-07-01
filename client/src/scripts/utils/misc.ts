8a547b46e7b20f2612f926536e459cb825bfb374
Henry Sanger <henry@sanger.io>
2023-07-01 14:05:26 -0400

    fix: revert changes to requestFullscreen function

--- client/src/scripts/utils/misc.ts
+++ client/src/scripts/utils/misc.ts
@@ -14,7 +14,11 @@ export function randomKillWord(): string {
 
 export function requestFullscreen(): void {
     const elem = document.documentElement;
-    void elem.requestFullscreen?.().catch();
-    // @ts-expect-error webkitRequestFullscreen is a WebKit only function
-    void elem.webkitRequestFullscreen?.().catch();
+    if (elem.requestFullscreen) {
+        void elem.requestFullscreen().catch();
+    } else { // @ts-expect-error shut up eslint
+        if (elem.webkitRequestFullScreen) { // @ts-expect-error shut up eslint
+            void elem.webkitRequestFullScreen().catch();
+        }
+    }
 }
