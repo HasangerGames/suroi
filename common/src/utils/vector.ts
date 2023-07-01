b55f691f1c64959e2c69287a16d1fc53abe7d23b
Dominic <domlincog@gmail.com>
2023-07-01 10:09:40 -0400

    Changed comments to TSdoc format

--- common/src/utils/vector.ts
+++ common/src/utils/vector.ts
@@ -1,35 +1,66 @@
-// An interface to represent a 2D vector. The x and y values are coordinates in a 2D space.
+/**
+ * An interface to represent a 2D vector. The x and y values are coordinates in a 2D space.
+ */
 export interface Vector {
-    x: number  // Represents the horizontal (x-axis) coordinate of the Vector
-    y: number  // Represents the vertical (y-axis) coordinate of the Vector
+    x: number
+    y: number
 }
 
-// This function creates a new Vector. Takes in two numbers representing x and y coordinates and returns an object of type Vector.
+/**
+ * Creates a new Vector.
+ * @param x - The horizontal (x-axis) coordinate.
+ * @param y - The vertical (y-axis) coordinate.
+ * @returns A new Vector object with the provided x and y coordinates.
+ */
 export function v(x: number, y: number): Vector {
     return { x, y };
 }
 
-// This function adds two vectors together. It adds the x and y coordinates of each Vector and returns a new Vector.
+/**
+ * Adds two Vectors together.
+ * @param a - The first Vector.
+ * @param b - The second Vector.
+ * @returns A new Vector resulting from the addition of vectors a and b.
+ */
 export function vAdd(a: Vector, b: Vector): Vector {
     return v(a.x + b.x, a.y + b.y);
 }
 
-// This function subtracts one vector from another. It subtracts the x and y coordinates of Vector b from Vector a and returns a new Vector.
+/**
+ * Subtracts one Vector from another.
+ * @param a - The Vector to be subtracted from.
+ * @param b - The Vector to subtract.
+ * @returns A new Vector resulting from the subtraction of vector b from vector a.
+ */
 export function vSub(a: Vector, b: Vector): Vector {
     return v(a.x - b.x, a.y - b.y);
 }
 
-// This function multiplies a vector by a scalar (a single number). It multiplies the x and y coordinates of the Vector by the scalar and returns a new Vector.
+/**
+ * Multiplies a Vector by a scalar.
+ * @param a - The Vector to be multiplied.
+ * @param n - The scalar value to multiply the Vector by.
+ * @returns A new Vector resulting from the multiplication of vector a and scalar n.
+ */
 export function vMul(a: Vector, n: number): Vector {
     return v(a.x * n, a.y * n);
 }
 
-// This function clones (or copies) a vector. It creates a new Vector with the same x and y coordinates as the input Vector.
+/**
+ * Clones a Vector.
+ * @param vector - The Vector to be cloned.
+ * @returns A new Vector with the same coordinates as the input Vector.
+ */
 export function vClone(vector: Vector): Vector {
     return v(vector.x, vector.y);
 }
 
-// This function rotates a vector by a given angle (in radians). It uses trigonometric functions to calculate the new coordinates after rotation and returns a new Vector.
+/**
+ * Rotates a Vector by a given angle.
+ * @param vector - The Vector to be rotated.
+ * @param angle - The angle in radians to rotate the Vector by.
+ * @returns A new Vector resulting from the rotation of the input Vector by the given angle.
+ */
 export function vRotate(vector: Vector, angle: number): Vector {
     const cos = Math.cos(angle); 
     const sin = Math.sin(angle);
