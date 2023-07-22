10f9f2ebd9d8540ec13ceb6a5897754802e450d5
Henry Sanger <henry@sanger.io>
2023-07-21 20:42:11 -0400

    feat: create definitions for all skins & emotes; add artist & dev skins; resize all emotes

---
+++ common/src/definitions/skins.ts
@@ -0,0 +1,112 @@
+import { type ObjectDefinition, ObjectDefinitions } from "../utils/objectDefinitions";
+
+export interface SkinDefinition extends ObjectDefinition {}
+
+export const Skins = new ObjectDefinitions<SkinDefinition>(
+    [
+        {
+            idString: "123op",
+            name: "123OP"
+        },
+        {
+            idString: "arctic_camo",
+            name: "Arctic Camo"
+        },
+        {
+            idString: "artist",
+            name: "Artist Swag"
+        },
+        {
+            idString: "beacon",
+            name: "Beacon"
+        },
+        {
+            idString: "deep_blue",
+            name: "Deep Blue"
+        },
+        {
+            idString: "desert_camo",
+            name: "Desert Camo"
+        },
+        {
+            idString: "dev",
+            name: "Developer Swag"
+        },
+        {
+            idString: "eipi",
+            name: "eiπ"
+        },
+        {
+            idString: "forest_camo",
+            name: "Forest Camo"
+        },
+        {
+            idString: "full_moon",
+            name: "Full Moon"
+        },
+        {
+            idString: "gunmetal",
+            name: "Gunmetal"
+        },
+        {
+            idString: "hasanger",
+            name: "Hasanger"
+        },
+        {
+            idString: "hyper_blue",
+            name: "Hyper Blue"
+        },
+        {
+            idString: "hyper_green",
+            name: "Hyper Green"
+        },
+        {
+            idString: "katie",
+            name: "Katie"
+        },
+        {
+            idString: "leia",
+            name: "Leia"
+        },
+        {
+            idString: "neon_green",
+            name: "Neon Green"
+        },
+        {
+            idString: "nokilpls",
+            name: "no kil pls"
+        },
+        {
+            idString: "pure_gold",
+            name: "Pure Gold"
+        },
+        {
+            idString: "pure_silver",
+            name: "Pure Silver"
+        },
+        {
+            idString: "red_hot",
+            name: "Red Hot"
+        },
+        {
+            idString: "solar_flare",
+            name: "Solar Flare"
+        },
+        {
+            idString: "sunset",
+            name: "Sunset"
+        },
+        {
+            idString: "swiss_cheese",
+            name: "Swiss Cheese"
+        },
+        {
+            idString: "target_practice",
+            name: "Target Practice"
+        },
+        {
+            idString: "zebra",
+            name: "Zebra"
+        }
+    ]
+);
