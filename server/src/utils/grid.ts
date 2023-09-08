9f07f4a53e867c1bd5d314ca9568ae09a30ac7eb
leia_uwu <leia@tutamail.com>
2023-09-07 21:10:33 -0300

    perf: optimize grid a bit

--- server/src/utils/grid.ts
+++ server/src/utils/grid.ts
@@ -11,13 +11,13 @@ export class Grid {
     private readonly _grid: Record<number, Record<number, Map<number, GameObject>>> = {};
 
     constructor(width: number, height: number, cellSize: number) {
-        this.width = width;
-        this.height = height;
+        this.width = Math.floor(width / cellSize);
+        this.height = Math.floor(height / cellSize);
         this.cellSize = cellSize;
 
-        for (let x = 0; x <= width; x += cellSize) {
+        for (let x = 0; x <= width; x++) {
             this._grid[x] = {};
-            for (let y = 0; y <= height; y += cellSize) {
+            for (let y = 0; y <= height; y++) {
                 this._grid[x][y] = new Map();
             }
         }
@@ -35,8 +35,8 @@ export class Grid {
             const max = this._roundToCells(rect.max);
 
             // add it to all grid cells that it intersects
-            for (let x = min.x; x <= max.x; x += this.cellSize) {
-                for (let y = min.y; y <= max.y; y += this.cellSize) {
+            for (let x = min.x; x <= max.x; x++) {
+                for (let y = min.y; y <= max.y; y++) {
                     this._grid[x][y].set(object.id, object);
                     object.gridCells.push(v(x, y));
                 }
@@ -64,8 +64,8 @@ export class Grid {
         const min = this._roundToCells(rect.min);
         const max = this._roundToCells(rect.max);
 
-        for (let x = min.x; x <= max.x; x += this.cellSize) {
-            for (let y = min.y; y <= max.y; y += this.cellSize) {
+        for (let x = min.x; x <= max.x; x++) {
+            for (let y = min.y; y <= max.y; y++) {
                 for (const [id, object] of this._grid[x][y]) {
                     if (!objectsAdded.get(id)) {
                         objectsAdded.set(id, true);
@@ -79,7 +79,7 @@ export class Grid {
     }
 
     private _roundToCells(vector: Vector): Vector {
-        return v(clamp(Math.floor(vector.x / this.cellSize) * this.cellSize, 0, this.width),
-            clamp(Math.floor(vector.y / this.cellSize) * this.cellSize, 0, this.height));
+        return v(clamp(Math.floor(vector.x / this.cellSize), 0, this.width),
+            clamp(Math.floor(vector.y / this.cellSize), 0, this.height));
     }
 }
