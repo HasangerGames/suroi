bc4e662116c841895d843e3497cfa4ff6f185fcc
leia_uwu <leia@tutamail.com>
2023-09-09 17:11:00 -0300

    fix: fix loot spawning issues in debug and arena map

--- server/src/objects/loot.ts
+++ server/src/objects/loot.ts
@@ -45,7 +45,7 @@ export class Loot extends GameObject {
     constructor(game: Game, type: ObjectType<ObjectCategory.Loot, LootDefinition>, position: Vector, count?: number) {
         super(game, type, position);
 
-        this.hitbox = new CircleHitbox(LootRadius[this.type.definition.itemType], position);
+        this.hitbox = new CircleHitbox(LootRadius[this.type.definition.itemType], vClone(position));
         this.oldPosition = this._position;
 
         if (count !== undefined) this.count = count;
