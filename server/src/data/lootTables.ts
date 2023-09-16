d21c46a278c19c2d35df824e846580d7750a5c6e
Katloo <132523318+Katloo24@users.noreply.github.com>
2023-09-16 17:22:10 -0400

    Update lootTables.ts
    Make scopes more rare

--- server/src/data/lootTables.ts
+++ server/src/data/lootTables.ts
@@ -9,7 +9,7 @@ export const LootTables: Record<string, LootTable> = {
         max: 1,
         loot: [
             { tier: "equipment", weight: 1 },
-            { tier: "scopes", weight: 1 },
+            { tier: "scopes", weight: 0.5 },
             { tier: "healing_items", weight: 1 },
             { tier: "ammo", weight: 1 },
             { tier: "guns", weight: 0.9 }
@@ -21,7 +21,7 @@ export const LootTables: Record<string, LootTable> = {
         loot: [
             { tier: "guns", weight: 1.25 },
             { tier: "equipment", weight: 1 },
-            { tier: "scopes", weight: 1 },
+            { tier: "scopes", weight: 0.5 },
             { tier: "healing_items", weight: 1 },
             { tier: "ammo", weight: 0.5 },
             { tier: "melee", weight: 0.04 }
@@ -76,7 +76,7 @@ export const LootTables: Record<string, LootTable> = {
         max: 1,
         loot: [
             { tier: "special_guns", weight: 1 },
-            { tier: "special_scopes", weight: 0.65 },
+            { tier: "special_scopes", weight: 0.35 },
             { tier: "special_equipment", weight: 0.65 }
         ]
     },
@@ -86,7 +86,7 @@ export const LootTables: Record<string, LootTable> = {
         loot: [
             { tier: "guns", weight: 1 },
             { tier: "equipment", weight: 0.65 },
-            { tier: "scopes", weight: 0.65 }
+            { tier: "scopes", weight: 0.325 }
         ]
     },
     small_drawer: {
@@ -113,7 +113,7 @@ export const LootTables: Record<string, LootTable> = {
             { tier: "healing_items", weight: 1 },
             { tier: "equipment", weight: 1 },
             { tier: "guns", weight: 0.5 },
-            { tier: "scopes", weight: 1 }
+            { tier: "scopes", weight: 0.5 }
         ]
     },
     bookshelf: {
@@ -121,7 +121,7 @@ export const LootTables: Record<string, LootTable> = {
         max: 2,
         loot: [
             { tier: "equipment", weight: 1.1 },
-            { tier: "scopes", weight: 1.1 },
+            { tier: "scopes", weight: 0.55 },
             { tier: "guns", weight: 1 },
             { tier: "healing_items", weight: 0.6 }
         ]
@@ -158,7 +158,7 @@ export const LootTables: Record<string, LootTable> = {
         loot: [
             { tier: "guns", weight: 1.25 },
             { tier: "equipment", weight: 1 },
-            { tier: "scopes", weight: 1 },
+            { tier: "scopes", weight: 0.5 },
             { tier: "special_guns", weight: 0.8 },
             { tier: "healing_items", weight: 0.75 }
         ]
@@ -171,7 +171,7 @@ export const LootTables: Record<string, LootTable> = {
             { tier: "healing_items", weight: 1 },
             { tier: "equipment", weight: 0.9 },
             { tier: "special_guns", weight: 0.8 },
-            { tier: "special_scopes", weight: 0.75 }
+            { tier: "special_scopes", weight: 0.5 }
         ]
     },
     porta_potty_toilet_closed: {
