134841eabd630941708511bbba5358c7fefee3c9
PythonCat <119687855+pythoncat1@users.noreply.github.com>
2023-10-04 14:50:13 -0400

    Update config.ts

--- server/src/config.ts
+++ server/src/config.ts
@@ -143,13 +143,13 @@ export const Config = {
     censorUsernames: true,
 
     roles: {
-        dev: { password: "dev" },
-        artist: { password: "artist", noPrivileges: true },
-        hasanger: { password: "hasanger" },
-        leia: { password: "leia" },
-        katie: { password: "katie" },
-        eipi: { password: "eipi" },
-        "123op": { password: "123op" },
-        radians: { password: "radians" }
+        // dev: { password: "dev" },
+        // artist: { password: "artist", noPrivileges: true },
+        // hasanger: { password: "hasanger" },
+        // leia: { password: "leia" },
+        // katie: { password: "katie" },
+        // eipi: { password: "eipi" },
+        // "123op": { password: "123op" },
+        // radians: { password: "radians" }
     }
 } satisfies ConfigType as ConfigType;
