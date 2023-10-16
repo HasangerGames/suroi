097b0cd7107363cd4ab2f1e1e889ed6745a3a331
Henry Sanger <henry@sanger.io>
2023-10-16 19:09:06 -0400

    fix: missing config

---
+++ client/src/scripts/config.ts
@@ -0,0 +1,19 @@
+export const Config = {
+    regions: {
+        dev: { name: "Local Server", address: "127.0.0.1:8000", https: false },
+        na: { name: "North America", address: "suroi.io", https: true },
+        eu: { name: "Europe", address: "eu.suroi.io", https: true },
+        sa: { name: "South America", address: "sa.suroi.io", https: true },
+        as: { name: "Asia", address: "as.suroi.io", https: true }
+    },
+    defaultRegion: "na"
+} satisfies ConfigType as ConfigType;
+
+export interface ConfigType {
+    readonly regions: Record<string, {
+        name: string
+        address: string
+        https: boolean
+    }>
+    readonly defaultRegion: string
+}
