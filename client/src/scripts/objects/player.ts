e4047f759004b3517eb38690ad4aabda422c7160
Henry Sanger <henry@sanger.io>
2023-10-17 10:05:22 -0400

    fix: hit effect

--- client/src/scripts/objects/player.ts
+++ client/src/scripts/objects/player.ts
@@ -668,7 +668,7 @@ export class Player extends GameObject {
 
         this.game.particleManager.spawnParticle({
             frames: "blood_particle",
-            zIndex: zIndexes.Players + 1,
+            zIndex: ZIndexes.Players + 1,
             position,
             lifeTime: 1000,
             scale: {
