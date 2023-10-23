6970b631dd78df161b1f59b894f323d0f039507e
Henry Sanger <henry@sanger.io>
2023-10-23 16:20:16 -0400

    fix: bad word filter

--- server/src/utils/badWordFilter.ts
+++ server/src/utils/badWordFilter.ts
@@ -1,6 +1,7 @@
-const badWordRegexes: string[] = [
-    /n[i1!]+[g693]+[e3]*[ra@]*/i,
-    /f[a@4]+[g9]+[s$5z2]+[o0]+[t+]*/i
+/* eslint-disable no-multi-spaces */
+const badWordRegexes: RegExp[] = [
+    /n[i1!]+[g693]+[e3]*[ra@]*/i,     // N word
+    /f[a@4]+[g9]+[s$5z2]+[o0]+[t+]*/i // F slur
 ];
 
 export function hasBadWords(text: string): boolean {
