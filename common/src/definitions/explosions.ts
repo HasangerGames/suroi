12ce2c32e032b69f776d0e614fd88fd3763d7f34
Henry Sanger <henry@sanger.io>
2023-10-26 19:55:54 -0400

    enhance: balance USAS

--- common/src/definitions/explosions.ts
+++ common/src/definitions/explosions.ts
@@ -199,8 +199,8 @@ export const Explosions = new ObjectDefinitions<ExplosionDefinition>(
             damage: 50,
             obstacleMultiplier: 2,
             radius: {
-                min: 4,
-                max: 12
+                min: 6,
+                max: 16
             },
             cameraShake: {
                 duration: 100,
@@ -211,9 +211,9 @@ export const Explosions = new ObjectDefinitions<ExplosionDefinition>(
                 tint: 0x6c1313,
                 scale: 0.8
             },
-            shrapnelCount: 8,
+            shrapnelCount: 13,
             ballistics: {
-                damage: 7.5,
+                damage: 3,
                 obstacleMultiplier: 2,
                 speed: 0.06,
                 maxDistance: 10,
