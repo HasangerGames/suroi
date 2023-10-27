3d5f2572b21b6fab78eb27326e371f817d3c72e0
Henry Sanger <henry@sanger.io>
2023-10-26 21:53:55 -0400

    refactor: move USAS to event only section of guns.ts

--- common/src/definitions/guns.ts
+++ common/src/definitions/guns.ts
@@ -328,44 +328,6 @@ export const Guns: GunDefinition[] = [
         }
     },
     {
-        idString: "usas12",
-        name: "USAS-12",
-        itemType: ItemType.Gun,
-        ammoType: "12g",
-        ammoSpawnAmount: 20,
-        capacity: 10,
-        reloadTime: 2,
-        fireDelay: 250,
-        switchDelay: 250,
-        speedMultiplier: 0.9,
-        recoilMultiplier: 0.8,
-        recoilDuration: 100,
-        fireMode: FireMode.Auto,
-        shotSpread: 3,
-        moveSpread: 3,
-        length: 8.2,
-        fists: {
-            left: v(120, 0),
-            right: v(70, -3),
-            animationDuration: 100
-        },
-        image: { position: v(100, -4) },
-        casingParticles: {
-            position: v(4, 0.6)
-        },
-        ballistics: {
-            damage: 8,
-            obstacleMultiplier: 1,
-            speed: 0.16,
-            maxDistance: 100,
-            onHitExplosion: "usas_explosion",
-            goToMouse: true,
-            tracer: {
-                color: 0xFF0000
-            }
-        }
-    },
-    {
         idString: "mosin",
         name: "Mosin-Nagant",
         itemType: ItemType.Gun,
@@ -926,6 +888,44 @@ export const Guns: GunDefinition[] = [
 
     // only event weapons below this point
 
+    {
+        idString: "usas12",
+        name: "USAS-12",
+        itemType: ItemType.Gun,
+        ammoType: "12g",
+        ammoSpawnAmount: 20,
+        capacity: 10,
+        reloadTime: 2,
+        fireDelay: 250,
+        switchDelay: 250,
+        speedMultiplier: 0.9,
+        recoilMultiplier: 0.8,
+        recoilDuration: 100,
+        fireMode: FireMode.Auto,
+        shotSpread: 3,
+        moveSpread: 3,
+        length: 8.2,
+        fists: {
+            left: v(120, 0),
+            right: v(70, -3),
+            animationDuration: 100
+        },
+        image: { position: v(100, -4) },
+        casingParticles: {
+            position: v(4, 0.6)
+        },
+        ballistics: {
+            damage: 8,
+            obstacleMultiplier: 1,
+            speed: 0.16,
+            maxDistance: 100,
+            onHitExplosion: "usas_explosion",
+            goToMouse: true,
+            tracer: {
+                color: 0xFF0000
+            }
+        }
+    },
     {
         idString: "s_g17",
         name: "G17 (scoped)",
