2fa8eb2fb44ee34c2872e81a13462ea4d8c2714b
Henry Sanger <henry@sanger.io>
2023-11-19 15:41:05 -0500

    feat: add Vietnam server; remove US west server

--- client/src/scripts/config.ts
+++ client/src/scripts/config.ts
@@ -1,11 +1,11 @@
 export const Config = {
     regions: {
         dev: { name: "Local Server", address: "127.0.0.1:8000", https: false },
-        na: { name: "US East", address: "na.suroi.io", https: true },
-        na2: { name: "US West", address: "na2.suroi.io", https: true },
+        na: { name: "North America", address: "na.suroi.io", https: true },
         eu: { name: "Europe", address: "eu.suroi.io", https: true },
         sa: { name: "South America", address: "sa.suroi.io", https: true },
-        as: { name: "Asia", address: "as.suroi.io", https: true }
+        as: { name: "Asia", address: "as.suroi.io", https: true },
+        vn: { name: "Vietnam", address: "vn.suroi.io", https: true }
     },
     defaultRegion: "na"
 } satisfies ConfigType as ConfigType;
