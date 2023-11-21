5435039e8730433f91ec6ea248e05d867240d434
Henry Sanger <henry@sanger.io>
2023-11-21 15:07:59 -0500

    fix: bump protocol version

--- common/src/constants.ts
+++ common/src/constants.ts
@@ -84,7 +84,7 @@ export enum PlayerActions {
 
 // !!!!! NOTE: Increase this every time a bit stream change is made between latest release and master
 // or a new item is added to a definition list
-export const PROTOCOL_VERSION = 5;
+export const PROTOCOL_VERSION = 6;
 
 export const MIN_OBJECT_SCALE = 0.25;
 export const MAX_OBJECT_SCALE = 2;
