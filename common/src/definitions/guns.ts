40e1b8d2fa636b68ee386fcc352ea872bfe229ec
Katloo <132523318+Katloo24@users.noreply.github.com>
2023-11-25 17:33:44 -0500

    funny

--- common/src/definitions/guns.ts
+++ common/src/definitions/guns.ts
@@ -354,7 +354,7 @@ export const Guns: GunDefinition[] = [
             damage: 10,
             obstacleMultiplier: 1,
             speed: 0.16,
-            range: 40,
+            range: 48,
             tracer: {
                 length: 0.5
             }
@@ -371,7 +371,7 @@ export const Guns: GunDefinition[] = [
         singleReload: true,
         fireDelay: 900,
         switchDelay: 900,
-        speedMultiplier: 0.9,
+        speedMultiplier: 0.92,
         recoilMultiplier: 0.45,
         recoilDuration: 750,
         fireMode: FireMode.Single,
@@ -411,7 +411,7 @@ export const Guns: GunDefinition[] = [
         reloadTime: 2.6,
         fireDelay: 900,
         switchDelay: 900,
-        speedMultiplier: 0.9,
+        speedMultiplier: 0.92,
         recoilMultiplier: 0.4,
         recoilDuration: 1000,
         fireMode: FireMode.Single,
