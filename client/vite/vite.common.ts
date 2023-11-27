1418ec1ad018e4b5cf04c6314f321ee1f2189672
leia uwu <leia@tutamail.com>
2023-11-26 23:33:10 -0300

    fix: client pages

--- client/vite/vite.common.ts
+++ client/vite/vite.common.ts
@@ -3,8 +3,21 @@ import pkg from "../../package.json";
 import { splitVendorChunkPlugin, type UserConfig } from "vite";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import { spritesheet } from "vite-spritesheet-plugin";
+import { resolve } from "path";
 
 const config: UserConfig = {
+
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "../index.html"),
+                changelog: resolve(__dirname, "../changelog/index.html"),
+                news: resolve(__dirname, "../news/index.html"),
+                rules: resolve(__dirname, "../rules/index.html")
+            }
+        }
+    },
+
     plugins: [
         splitVendorChunkPlugin(),
         ViteImageOptimizer({
