8854023c13c2c1e7ae839f8d43f8506389ac5414
Henry Sanger <henry@sanger.io>
2023-11-27 09:42:39 -0500

    fix: oops

--- server/src/objects/player.ts
+++ server/src/objects/player.ts
@@ -359,8 +359,6 @@ export class Player extends GameObject<ObjectCategory.Player> {
             this.inventory.scope = "4x_scope";
         }
 
-        this.giveGun("radio");
-
         this.updateAndApplyModifiers();
         this.dirty.weapons = true;
     }
