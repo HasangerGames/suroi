10d3c0577c0e2aa32971f66c7c88bf11fc832637
leia uwu <leia@tutamail.com>
2023-12-27 18:21:49 -0300

    fix: some river generation fixes

--- server/src/map.ts
+++ server/src/map.ts
@@ -243,7 +243,11 @@ export class Map {
         }
         if (riverPoints.length < 20 || riverPoints.length > 59) return;
 
-        rivers.push(new River(width, riverPoints, rivers));
+        const mapBounds = new RectangleHitbox(
+            Vec.create(this.oceanSize, this.oceanSize),
+            Vec.create(this.width - this.oceanSize, this.height - this.oceanSize)
+        );
+        rivers.push(new River(width, riverPoints, rivers, mapBounds));
     }
 
     generateBuildings(definition: ReifiableDef<BuildingDefinition>, count: number): void {
