fb1af35c5cf3edc783792dcc5538a3f6e5753572
Henry Sanger <henry@sanger.io>
2024-03-03 19:05:52 -0500

    fix: missing control panel killfeed icon

--- server/src/data/maps.ts
+++ server/src/data/maps.ts
@@ -355,7 +355,7 @@ export const Maps: Record<string, MapDefinition> = {
         beachSize: 8,
         oceanSize: 8,
         genCallback(map) {
-            map.generateObstacle("barrel", Vec.create(this.width / 2, this.height / 2), 0);
+            map.generateObstacle("control_panel", Vec.create(this.width / 2, this.height / 2), 0);
         }
     },
     singleGun: {
