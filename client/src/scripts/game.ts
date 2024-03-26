ea9d30c78d07cd8bdb064cd7f3640fc455961e87
PythonCat <119687855+pythoncat1@users.noreply.github.com>
2024-03-25 23:24:10 -0400

    Only autopickup of auto pickup cvar is true

--- client/src/scripts/game.ts
+++ client/src/scripts/game.ts
@@ -794,13 +794,15 @@ export class Game {
                     this.inputManager.isMobile &&
                     canInteract &&
                     (
-                        ( // Auto pickup
-                            object instanceof Loot &&
-                            // Only pick up melees if no melee is equipped
-                            (type !== ItemType.Melee || this.uiManager.inventory.weapons?.[2]?.definition.idString === "fists") &&
-                            // Only pick up guns if there's a free slot
-                            (type !== ItemType.Gun || (!this.uiManager.inventory.weapons?.[0] || !this.uiManager.inventory.weapons?.[1])) &&
-                            type !== ItemType.Skin
+                        ( this.console.getBuiltInCVar("cv_auto_pickup") && (
+                                // Auto pickup
+                                object instanceof Loot &&
+                                // Only pick up melees if no melee is equipped
+                                (type !== ItemType.Melee || this.uiManager.inventory.weapons?.[2]?.definition.idString === "fists") &&
+                                // Only pick up guns if there's a free slot
+                                (type !== ItemType.Gun || (!this.uiManager.inventory.weapons?.[0] || !this.uiManager.inventory.weapons?.[1])) &&
+                                type !== ItemType.Skin
+                            )
                         ) ||
                         ( // Auto open doors
                             object instanceof Obstacle &&
