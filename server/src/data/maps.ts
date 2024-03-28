90cb10f3c256dfd2b7e4aea0ea05c456bc4a949b
Henry Sanger <henry@sanger.io>
2024-03-27 21:53:16 -0400

    fix: potted plant particle

--- server/src/data/maps.ts
+++ server/src/data/maps.ts
@@ -359,7 +359,7 @@ export const Maps: Record<string, MapDefinition> = {
         beachSize: 8,
         oceanSize: 8,
         genCallback(map) {
-            map.generateObstacle("house_wall_9", Vec.create(this.width / 2, this.height / 2), 0);
+            map.generateObstacle("potted_plant", Vec.create(this.width / 2, this.height / 2), 0);
         }
     },
     singleGun: {
