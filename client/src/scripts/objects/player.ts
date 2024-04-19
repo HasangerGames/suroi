27903115e3bb4243a01a0276e764c83a482e7f2e
Henry Sanger <henry@sanger.io>
2024-04-19 17:05:13 -0400

    fix: visual glitch with knocked out players holding weapons

--- client/src/scripts/objects/player.ts
+++ client/src/scripts/objects/player.ts
@@ -478,16 +478,9 @@ export class Player extends GameObject<ObjectCategory.Player> {
             this.container.alpha = full.invulnerable ? 0.5 : 1;
 
             if (this.downed !== full.downed) {
-                updateContainerZIndex = true;
                 this.downed = full.downed;
-                this.updateFistsPosition(false);
-                this.updateWeapon(isNew);
+                updateContainerZIndex = true;
                 this.updateEquipment();
-
-                if (!this.dead) {
-                    this.updateFistsPosition(false);
-                    this.updateWeapon(isNew);
-                }
             }
 
             if (this.beingRevived !== full.beingRevived) {
@@ -555,13 +548,12 @@ export class Player extends GameObject<ObjectCategory.Player> {
             this.vestLevel = (this.equipment.vest = full.vest)?.level ?? 0;
             this.backpackLevel = (this.equipment.backpack = full.backpack).level;
 
-            const equipmentDirty =
+            if (
                 hideEquipment !== this.hideEquipment ||
                 helmetLevel !== this.helmetLevel ||
                 vestLevel !== this.vestLevel ||
-                backpackLevel !== this.backpackLevel;
-
-            if (equipmentDirty) {
+                backpackLevel !== this.backpackLevel
+            ) {
                 this.updateEquipment();
             }
 
@@ -569,6 +561,13 @@ export class Player extends GameObject<ObjectCategory.Player> {
                 this.updateFistsPosition(true);
                 this.updateWeapon(isNew);
             }
+        }
+
+        // fixme hack to prevent visual glitches when downed
+        // The ThrowableCook animation seems to be causing the issues
+        if (this.downed) {
+            this.updateFistsPosition(false);
+            this.updateWeapon(isNew);
         }
 
         if (updateContainerZIndex) {
