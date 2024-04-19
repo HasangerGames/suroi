9868a2c3bcae438efd9b6d3a7a44823dd71cc72e
Henry Sanger <henry@sanger.io>
2024-04-18 22:03:21 -0400

    fix: make nades drop when knocked out

--- server/src/objects/player.ts
+++ server/src/objects/player.ts
@@ -1419,6 +1419,7 @@ export class Player extends BaseGameObject<ObjectCategory.Player> {
 
         this.downed = true;
         this.action?.cancel();
+        this.activeItem.stopUse();
         this.health = 100;
         this.adrenaline = this.minAdrenaline;
         this.setDirty();
