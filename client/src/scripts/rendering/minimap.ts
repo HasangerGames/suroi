8a5b5ceda7c6b32da0b40666ce3ced5b5b5b30db
pap-24 <71824952+pap-24@users.noreply.github.com>
2024-05-01 22:19:10 +0300

    Bug Fix: Kill feed acting weird while the "big" map is opened.

--- client/src/scripts/rendering/minimap.ts
+++ client/src/scripts/rendering/minimap.ts
@@ -584,6 +584,13 @@ export class Minimap {
         $("#btn-close-minimap").show();
         $("#ui-kill-leader").hide();
         $("#center-bottom-container").hide();
+
+        // ----------------------
+        // Bug Fix
+        // ----------------------
+        $("#kill-feed").hide();
+        // ----------------------
+
         $("#kill-counter").show();
         this.resize();
     }
@@ -594,6 +601,13 @@ export class Minimap {
         $("#center-bottom-container").show();
         $("#gas-msg-info").show();
         $("#scopes-container").show();
+
+        // ----------------------
+        // Bug Fix
+        // ----------------------
+        $("#kill-feed").show();
+        // ----------------------
+
         if (this.game.spectating) $("#spectating-container").show();
         const width = $(window).width();
         if (width && width > 1200) $("#ui-kill-leader").show();
