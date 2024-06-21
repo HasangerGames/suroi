b180282d67892e5ca7d38d14383e6627718e2146
Kenos <flatware-gold-bronze@tutanota.com>
2024-06-21 12:23:06 -0600

    enhance: make translation function not require language choice

--- client/src/translations.ts
+++ client/src/translations.ts
@@ -8,12 +8,34 @@ export const TRANSLATIONS = {
             name: "English",
             msg_rotate: "For a better experience, please rotate your device to landscape.",
             msg_loading: "Connecting",
+
             play_solo: "Play Solo",
             play_duo: "Play Duos",
             play_squad: "Play Squads",
             join_team: "Join Team",
             create_team: "Create Team",
-            msg_locked_tooltip: "The game switches between solos and duos every 24 hours, as there aren't enough players for both."
+            msg_locked_tooltip: "The game switches between solos and duos every 24 hours, as there aren't enough players for both.",
+            rules_and_tutorial: "Rules & Tutorial",
+            loadout: "Loadout",
+            settings: "Settings",
+
+            featured_youtubr: "Featured Youtubr",
+            featured_streamr: "Featured Streamr",
+
+            btn_report: "Report",
+            btn_spectate_kill_leader: "Spectate Kill Leader",
+            btn_spectate: "Spectate",
+            btn_play_again: "Play Again",
+            btn_menu: "Menu",
+
+            msg_waiting_for_leader: "Waiting for leader",
+            msg_you_died: "You died.",
+            msg_your_rank: "Rank",
+
+            go_kills: "Kills:",
+            go_damage_done: "Damage Done:",
+            go_damage_taken: "Damage Taken:",
+            go_time_alive: "Time Alive:"
         },
         hp18: {
             name: "HP-18",
@@ -26,20 +48,26 @@ export const TRANSLATIONS = {
     readonly translations: Record<string, Record<string, string> & {name: string}>
 }
 
-export function getTranslatedString(id: string, language: string, replacements?: Record<string, string>) {
+const localStorage = JSON.parse(window.localStorage.getItem("suroi_config") ?? "{}")
+
+export function getTranslatedString(id: string, replacements?: Record<string, string>) {
+    const language = localStorage["variables"]["cv_language"] ?? defaultClientCVars["cv_language"]
+
     let foundTranslation = TRANSLATIONS["translations"][language][id]
         ?? TRANSLATIONS["translations"][TRANSLATIONS.defaultLanguage][id]
         ?? Loots.reify(id).name;
+
+    if (!foundTranslation) return "";
+
     if (!replacements) {
         return foundTranslation;
     }
+
     for (const [search, replace] of Object.entries(replacements)) {
         foundTranslation = foundTranslation.replaceAll(`<${search}>`, replace);
     }
     return foundTranslation;
 }
-
-const localStorage = JSON.parse(window.localStorage.getItem("suroi_config") ?? "{}")
 
 let debugTranslationCounter = 0
 
@@ -47,7 +75,7 @@ document.querySelectorAll("body *").forEach((element) => {
     const requestedTranslation = element.getAttribute("translation")
     if (!requestedTranslation) return;
 
-    element.innerHTML = getTranslatedString(requestedTranslation, localStorage["variables"]["cv_language"] ?? defaultClientCVars["cv_language"])
+    (element as HTMLDivElement).innerText = getTranslatedString(requestedTranslation)
     debugTranslationCounter++
 })
 
