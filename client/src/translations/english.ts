4d95e8e4be26a5d3f458d834b80578b7ee36d7df
Kenos <flatware-gold-bronze@tutanota.com>
2024-06-23 11:14:01 -0600

    enhance: move english translations to a new file

---
+++ client/src/translations/english.ts
@@ -0,0 +1,58 @@
+import type { TranslationMap } from "../translations";
+
+export const ENGLISH_TRANSLATIONS: TranslationMap = {
+    name: "English",
+    flag: "🇬🇧",
+
+    msg_rotate: "For a better experience, please rotate your device to landscape.",
+    msg_loading: "Connecting",
+
+    play_solo: "Play Solo",
+    play_duo: "Play Duos",
+    play_squad: "Play Squads",
+    join_team: "Join Team",
+    create_team: "Create Team",
+    msg_locked_tooltip: "The game switches between solos and duos every 24 hours, as there aren't enough players for both.",
+    rules_and_tutorial: "Rules & Tutorial",
+    loadout: "Loadout",
+    settings: "Settings",
+
+    featured_youtubr: "Featured Youtubr",
+    featured_streamr: "Featured Streamr",
+
+    btn_report: "Report",
+    btn_spectate_kill_leader: "Spectate Kill Leader",
+    btn_spectate: "Spectate",
+    btn_play_again: "Play Again",
+    btn_menu: "Menu",
+
+    msg_waiting_for_leader: "Waiting for leader",
+    msg_you_died: "You died.",
+    msg_player_died: "<player> died.",
+    msg_win: "Winner Winner Chicken Dinner!",
+    msg_your_rank: "Rank",
+
+    msg_kills: "Kills: <kills>",
+
+    kf_suicide_kill: "<player> commited suicide",
+    kf_suicide_downed: "<player> knocked themselves out",
+    kf_two_party_kill: "<player> killed <victim>",
+    kf_two_party_downed: "<player> knocked out <victim>",
+    kf_bleed_out_kill: "<player> bled out",
+    kf_bleed_out_downed: "<player bled out non-lethally",
+    kf_finished_off_kill: "<player> finished off <victim>",
+    kf_finished_off_downed: "<player> gently finished off <victim>",
+    kf_finally_killed: "<player> was finally killed",
+    kf_finally_downed: "<player> was finally knocked out",
+    kf_gas_kill: "<player> died to the gas",
+    kf_gas_downed: "<player> was knocked out by the gas",
+    kf_airdrop_kill: "<player> was fatally crushed by an airdrop",
+    kf_airdrop_downed: "<player> was knocked out by an airdrop",
+
+    go_kills: "Kills:",
+    go_damage_done: "Damage Done:",
+    go_damage_taken: "Damage Taken:",
+    go_time_alive: "Time Alive:",
+
+    languages: "Languages"
+}
