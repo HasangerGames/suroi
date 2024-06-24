9d2efe5bca54aefe5eeecb4397ce4b17e9ed75b2
pap-24 <sotirispapagiannis1@gmail.com>
2024-06-24 16:46:30 +0300

    Fix: if condition format

--- server/src/objects/obstacle.ts
+++ server/src/objects/obstacle.ts
@@ -148,8 +148,18 @@ export class Obstacle extends BaseGameObject<ObjectCategory.Obstacle> {
 
         const weaponDef = weaponUsed instanceof InventoryItem ? weaponUsed.definition : undefined;
 
-        // please beautify this if condition without eslint getting angry
-        if (definition.impenetrable && !((weaponDef?.itemType === ItemType.Melee && weaponDef.piercingMultiplier !== undefined && weaponDef.canPierceMaterials !== undefined && weaponDef.canPierceMaterials.includes(this.definition.material)) || source instanceof Obstacle)) {
+        if (
+            definition.impenetrable &&
+            !(
+                (
+                    weaponDef?.itemType === ItemType.Melee
+                    && weaponDef.piercingMultiplier !== undefined
+                    && weaponDef.canPierceMaterials !== undefined
+                    && weaponDef.canPierceMaterials.includes(this.definition.material)
+                )
+                || source instanceof Obstacle
+            )
+        ) {
             return;
         }
 
