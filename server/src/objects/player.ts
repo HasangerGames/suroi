c3bdbe35b0c0825924dffd21f8e5ce64b4d6846c
Leon <107775211+Leon-9982@users.noreply.github.com>
2024-06-24 12:08:51 +0100

    Change in adren healing speed

--- server/src/objects/player.ts
+++ server/src/objects/player.ts
@@ -701,10 +701,10 @@ export class Player extends BaseGameObject<ObjectCategory.Player> {
         }
 
         // Regenerate health
-        if (this._adrenaline >= 87.5) this.health += dt * 2.75 / (30 ** 2);
-        else if (this._adrenaline >= 50) this.health += dt * 2.125 / (30 ** 2);
-        else if (this._adrenaline >= 25) this.health += dt * 1.125 / (30 ** 2);
-        else if (this._adrenaline > 0) this.health += dt * 0.625 / (30 ** 2);
+        if (this._adrenaline >= 87.5) this.health += dt * 5 / (50 ** 2);
+        else if (this._adrenaline >= 50) this.health += dt * 4.75 / (50 ** 2);
+        else if (this._adrenaline >= 25) this.health += dt * 3.75 / (50 ** 2);
+        else if (this._adrenaline > 0) this.health += dt * 1 / (50 ** 2);
 
         // Shoot gun/use item
         if (this.startedAttacking) {
