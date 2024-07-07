08713b5a473a589018c308e9a458f2e09f3e7b8b
pap-24 <sotirispapagiannis1@gmail.com>
2024-07-07 19:33:56 +0300

    fix: error said no so uh bye badge

--- server/src/config.ts
+++ server/src/config.ts
@@ -51,7 +51,6 @@ export const Config = {
         "composr": { password: "composr" },
         "lead_composr": { password: "lead_composr" },
         "youtubr": { password: "youtubr" },
-        "translatr": { password: "translatr" },
         "boostr": { password: "boostr" },
 
         "hasanger": { password: "hasanger", isDev: true },
