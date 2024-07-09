28d457a376a43e0bde3176d9155ccdd9245433e8
pap-24 <sotirispapagiannis1@gmail.com>
2024-07-09 12:23:44 +0300

    gift for 109

--- client/src/scripts/ui.ts
+++ client/src/scripts/ui.ts
@@ -145,7 +145,7 @@ export async function setUpUI(game: Game): Promise<void> {
         languageFieldset.append(html`
             <div>
               <input type="radio" name="selected-language" id="language-${language}" value="${language}">
-              <label for="language-${language}">${languageInfo.flag} ${languageInfo.name} (${languageInfo.name === "HP-18" ? "HP-18" : Math.ceil(percentage * 100)}%)</label>
+              <label for="language-${language}">${languageInfo.flag} ${languageInfo.name} (${language === "qen" ? "OwO" : languageInfo.name === "HP-18" ? "HP-18" : Math.ceil(percentage * 100)}%)</label>
             </div>
         `);
 
