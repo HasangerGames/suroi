e3243a74eb143038651a460035bdfe24209dbdc4
Arman-AIi <armanali.aban@gmail.com>
2024-10-11 17:19:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require('electron/main')
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
+    width: 1920,
+    height: 1080
   })
 
   win.loadFile('client/index.html')
