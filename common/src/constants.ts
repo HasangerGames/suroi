a91594f63b555e8f67dbb50887a72b1bc85bae5f
Henry Sanger <henry@sanger.io>
2024-10-27 20:49:47 -0400

    chore: bump version numbers

--- common/src/constants.ts
+++ common/src/constants.ts
@@ -25,7 +25,7 @@ const inventorySlotTypings = Object.freeze([ItemType.Gun, ItemType.Gun, ItemType
 export const GameConstants = freezeDeep({
     // !!!!! NOTE: Increase this every time a bit stream change is made between latest release and master
     // or a new item is added to a definition list
-    protocolVersion: 27,
+    protocolVersion: 28,
     gridSize: 32,
     maxPosition: 1924,
     modeName: "fall" satisfies Mode as Mode,
