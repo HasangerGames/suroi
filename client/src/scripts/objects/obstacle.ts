21a7cb56bd54099b39088d3c55c49d37cb0c09d2
pap-24 <71824952+pap-24@users.noreply.github.com>
2024-11-09 13:47:34 +0200

    fix: layering issues (zIndex) with obstacle residues (i hope)
    the method was called before the (isDead) check so that might have caused the issue..

--- client/src/scripts/objects/obstacle.ts
+++ client/src/scripts/objects/obstacle.ts
@@ -207,8 +207,6 @@ export class Obstacle extends GameObject.derive(ObjectCategory.Obstacle) {
 
         this.container.scale.set(this.dead ? 1 : this.scale);
 
-        this.updateZIndex();
-
         if (isNew) {
             if (definition.glow !== undefined) {
                 const glow = definition.glow;
@@ -330,6 +328,8 @@ export class Obstacle extends GameObject.derive(ObjectCategory.Obstacle) {
             this._glow?.kill();
         }
 
+        this.updateZIndex();
+
         if (this._door === undefined) {
             this.hitbox = definition.hitbox.transform(this.position, this.scale, this.orientation);
         }
@@ -368,6 +368,7 @@ export class Obstacle extends GameObject.derive(ObjectCategory.Obstacle) {
                 ? ZIndexes.UnderWaterDeadObstacles
                 : ZIndexes.DeadObstacles
             : this.definition.zIndex ?? ZIndexes.ObstaclesLayer1;
+
         this.container.zIndex = getEffectiveZIndex(zIndex, this.layer, this.game.layer);
 
         // hides bunker doors on ground layer
