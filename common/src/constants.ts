f669460377a201282ff3c0177c7b20b124543a47
Henry Sanger <henry@sanger.io>
2024-12-23 20:03:34 -0500

    revert to winter mode

--- common/src/constants.ts
+++ common/src/constants.ts
@@ -43,7 +43,7 @@ const inventorySlotTypings = Object.freeze([ItemType.Gun, ItemType.Gun, ItemType
 export const GameConstants = freezeDeep({
     // !!!!! NOTE: Increase this every time a bit stream change is made between latest release and master
     // or a new item is added to a definition list
-    protocolVersion: 40,
+    protocolVersion: 41,
     gridSize: 32,
     maxPosition: Constants.MAX_POSITION,
     modeName: "fall" satisfies Mode as Mode,
