57d067bef24d46c374facc22d58b132debb22fae
Zereshk <zereshk.dev@gmail.com>
2024-12-29 15:49:42 -0800

    experimental: haptic effects near explosions

--- client/src/scripts/objects/explosion.ts
+++ client/src/scripts/objects/explosion.ts
@@ -71,6 +71,10 @@ export function explosion(game: Game, definition: ExplosionDefinition, position:
         );
     }
 
+    navigator.vibrate(
+        definition.animation.duration * 0.75
+    );
+
     if (definition.sound !== undefined) {
         game.soundManager.play(
             definition.sound,
