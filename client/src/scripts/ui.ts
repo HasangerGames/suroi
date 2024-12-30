2d81ed84e8cd7f8b3b1cb5592da269d38b6e3145
Zereshk <zereshk.dev@gmail.com>
2024-12-29 16:45:55 -0800

    feat: link toggle with cv

--- client/src/scripts/ui.ts
+++ client/src/scripts/ui.ts
@@ -1634,6 +1634,7 @@ export async function setUpUI(game: Game): Promise<void> {
     addCheckboxListener("#toggle-mobile-controls", "mb_controls_enabled");
     addSliderListener("#slider-joystick-size", "mb_joystick_size");
     addSliderListener("#slider-joystick-transparency", "mb_joystick_transparency");
+    addCheckboxListener("#toggle-haptics", "mb_haptics");
     addCheckboxListener("#toggle-high-res-mobile", "mb_high_res_textures");
 
     function updateUiScale(): void {
