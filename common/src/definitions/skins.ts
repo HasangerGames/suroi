1a15f49995883e2a6c01b49db977d9e61a70d82d
Zereshk <zereshk.dev@gmail.com>
2025-01-01 20:09:42 -0600

    enhance: adjust blue on sunrise fist

--- common/src/definitions/skins.ts
+++ common/src/definitions/skins.ts
@@ -174,7 +174,7 @@ export const Skins = ObjectDefinitions.withDefault<SkinDefinition>()(
                 ],
                 fistLayers: [
                     { frame: "plain_fist", tint: "#f06c48" },
-                    { frame: "plain_fist_gradient", tint: "#495f8c", alpha: 0.9, rotation: -Math.PI / 2 },
+                    { frame: "plain_fist_gradient", tint: "#4a608c", alpha: 0.9, rotation: -Math.PI / 2 },
                     { frame: "plain_fist_gradient", tint: "#f19341", alpha: 0.7, rotation: Math.PI / 2 },
                     { frame: "plain_fist_stroke", tint: "#191919" }
                 ]
