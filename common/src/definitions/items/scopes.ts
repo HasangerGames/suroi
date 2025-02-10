c01ff68299b8abf928a3121bb1cd446fb4a33a3a
Henry Sanger <henry@sanger.io>
2025-02-09 21:32:00 -0500

    enhance: rebalance scopes + rename 15x to 16x

--- common/src/definitions/items/scopes.ts
+++ common/src/definitions/items/scopes.ts
@@ -3,27 +3,26 @@ import { ItemType, ObjectDefinitions, type ItemDefinition } from "../../utils/ob
 export interface ScopeDefinition extends ItemDefinition {
     readonly itemType: ItemType.Scope
     readonly zoomLevel: number
-    readonly giveByDefault: boolean
+    readonly giveByDefault?: boolean
 }
 
 const scope = (magnification: string, zoomLevel: number): ScopeDefinition => ({
     idString: `${magnification}_scope`,
     name: `${magnification} Scope`,
     itemType: ItemType.Scope,
-    giveByDefault: false,
     zoomLevel
 });
 
 export const Scopes = new ObjectDefinitions<ScopeDefinition>([
     {
-        ...scope("1x", 70),
+        ...scope("1x", 80),
         noDrop: true,
         giveByDefault: true
     },
-    scope("2x", 100),
-    scope("4x", 135),
-    scope("8x", 185),
-    scope("15x", 255)
+    scope("2x", 110),
+    scope("4x", 140),
+    scope("8x", 170),
+    scope("16x", 220)
 ]);
 
 export const DEFAULT_SCOPE = Scopes.definitions[0];
