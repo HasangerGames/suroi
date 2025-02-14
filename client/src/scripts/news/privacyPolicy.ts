d9113c2819b933f62e82caa3afd077747c81147b
Zereshk <zereshk.dev@gmail.com>
2025-02-13 23:35:49 -0600

    feat: begin adding privacy policy page

---
+++ client/src/scripts/news/privacyPolicy.ts
@@ -0,0 +1,83 @@
+import { type RulesPost } from "./rulePosts";
+
+export const privacyPolicy: RulesPost = {
+        date: 1729944000000,
+        author: "Error",
+        title: "Policy",
+        content: "This document informs users of Suroi on how their personal data is collected and processed.",
+        longContent: `
+        <br>
+        <h2>1. Acceptance of Privacy Policy</h2>
+        By accessing or using suroi.io (the "Site" or "Game"), you agree to this Privacy Policy. If you do not agree with this policy, please do not use our Site. We comply with international data protection laws, including the General Data Protection Regulation (GDPR) for users in the European Union.
+        <br><br>
+
+        <h2>2. Information We Collect</h2>
+        <ul>
+          <li><h3>Account Information:</h3>
+            When you register an account using Discord, we collect your Discord username and associated email address.
+          </li>
+
+          <li><h3>Username:</h3>
+            If you register directly, we collect your chosen username and password.
+          </li>
+
+          <li><h3>IP Address:</h3>
+            We collect your IP address for security purposes, such as detecting and preventing unauthorized access or usage (e.g. VPN or proxy detection).
+          </li>
+
+          <li><h3>Usage Data:</h3>
+            We collect data on how much time you spend on the Site and how you interact with the Game to improve user experience.
+          </li>
+        </ul>
+
+        <br>
+
+        <h2>3. How We Use Your Information</h2>
+        <ul>
+          <li><h3>Account Management:</h3>
+            To create and manage your account, authenticate your access, and provide account support.\
+          </li>
+          <li><h3>Security:</h3>
+            To protect your account and the Site by detecting and preventing fraudulent or unauthorized activities, including the use of modified Game clients to provide a competitive advantage (known as “hacking”).
+          </li>
+          <li><h3>Account Recovery:</h3>
+          To assist you in recovering your account if you lose access.
+          </li>
+        </ul>
+
+        <br>
+
+        <h2>4. Cookies</h2>
+        We use cookies and similar technologies solely for authentication purposes:
+        <ul><li>Authentication Cookies: To keep you logged in and maintain your session on the Site.</li></ul>
+        You can disable cookies through your browser settings, but this may affect your ability to use the Site effectively.
+
+        <br><br>
+
+        <h2>5. Third-Party Services</h2>
+        We work with third-party service providers to operate and improve our services:
+        <ul>
+          <li><h3>Hosting Providers:</h3> We use Ionos, Hetzner, and GreenCloud for server hosting. These providers may process data on our behalf.
+
+          <li><h3>Cloudflare:</h3> To speed up the Site and provide protection against DDoS attacks. Refer to <a href="https://www.cloudflare.com/privacypolicy/">Cloudflare’s Privacy Policy</a> for more information.
+
+          <li><h3>Proxycheck.io:</h3> To detect VPNs and proxies for security purposes. Your IP address may be shared with Proxycheck.io. See <a href="https://proxycheck.io/privacy">Proxycheck.io's Privacy Policy</a> for details.
+
+          <li><h3>Discord:</h3> For account registration and authentication if you choose to sign up via Discord. Please refer to <a href="https://discord.com/privacy">Discord's Privacy Policy</a> for more information.
+
+        </ul>
+
+        <br>
+
+        <h2>6. Data Storage and Security</h2>
+        <ul>
+        <li><h3>Data Storage:</h3> Your data is stored securely on our servers hosted by our hosting providers.
+
+        <li><h3>Password Security:</h3> All passwords are securely hashed and not stored in plain text.
+
+        <li><h3>Access Control:</h3> Only essential team members (currently two people) have access to user data on a need-to-know basis.
+
+        <li><h3>Data Protection:</h3> We employ industry-standard security measures to protect your personal information from unauthorized access, alteration, disclosure, or destruction.
+        </ul>
+        `
+    }
