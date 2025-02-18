51896fc1c99380458fd4701ff9f6b568c450913b
Henry Sanger <henry@sanger.io>
2025-02-18 10:25:55 -0500

    fix: rules page; nuke privacy policy

--- client/privacy/src/main.ts
+++
@@ -1,7 +0,0 @@
-import "../../src/scss/pages/news.scss";
-import "../../node_modules/@fortawesome/fontawesome-free/css/brands.css";
-import "../../node_modules/@fortawesome/fontawesome-free/css/fontawesome.css";
-import "../../node_modules/@fortawesome/fontawesome-free/css/solid.css";
-import { createDropdown } from "../../src/scripts/uiHelpers";
-
-createDropdown("#splash-more");
