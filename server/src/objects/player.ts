0f0e47b293da17f0e7900908ccb1789c5033fc56
Henry Sanger <henry@sanger.io>
2025-02-24 21:57:04 -0500

    Revert "feat: always drop 2 boxes of ammo for each ammo type instead of many upon player death"
    This reverts commit eb4e387f86e0a84f2d425211a261a9c2622a2a7f.

--- server/src/objects/player.ts
+++ server/src/objects/player.ts
@@ -2309,8 +2309,14 @@ export class Player extends BaseGameObject.derive(ObjectCategory.Player) {
                 if (def.noDrop || ("ephemeral" in def && def.ephemeral)) continue;
 
                 if (def.itemType === ItemType.Ammo && count !== Infinity) {
-                    this.game.addLoot(item, position, layer, { count: Math.floor(count / 2) });
-                    this.game.addLoot(item, position, layer, { count: Math.ceil(count / 2) });
+                    let left = count;
+                    let subtractAmount = 0;
+
+                    do {
+                        left -= subtractAmount = Numeric.min(left, def.maxStackSize);
+                        this.game.addLoot(item, position, layer, { count: subtractAmount });
+                    } while (left > 0);
+
                     continue;
                 }
 
