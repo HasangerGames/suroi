713dcc7b0ea2526f8ec029770833e8a77b6ba5d2
Henry Sanger <henry@sanger.io>
2025-03-22 20:19:42 -0400

    fix: adrenaline not healing

--- server/src/objects/player.ts
+++ server/src/objects/player.ts
@@ -1171,7 +1171,7 @@ export class Player extends BaseGameObject.derive(ObjectCategory.Player) {
             toRegen += adrenRegen * this.mapPerkOrDefault(
                 PerkIds.LacedStimulants,
                 ({ healDmgRate, lowerHpLimit }) => (this.health <= lowerHpLimit ? 1 : -healDmgRate),
-                this.normalizedHealth < 0.3 && !this.downed ? 1 : 0 // passive regen
+                (this.adrenaline > 0 || this.normalizedHealth < 0.3) && !this.downed ? 1 : 0
             );
 
             // Drain adrenaline
