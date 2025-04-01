3c4a114ee4ef79b79da916eafe0b277ad41565a3
kenos <flatware-gold-bronze@tutanota.com>
2025-04-01 17:20:51 -0600

    fix: remove "JoinPacket not received after 5 seconds" error

--- client/src/changelog.ts
+++
@@ -1,1 +0,0 @@
-import "./scss/pages/changelog.scss";
--- client/src/scripts/managers/perkManager.ts
+++
@@ -1,7 +0,0 @@
-import { PerkManager, type PerkCollection } from "@common/utils/perkManager";
-
-export const ClientPerkManager = new (class ClientPerkManager extends PerkManager {
-    overwrite(perks: PerkCollection): void {
-        this._items = perks.asBitfield();
-    }
-})();
