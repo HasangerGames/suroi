da3388b4bd310e687f00278335a067fbde3fec8d
pap-24 <71824952+pap-24@users.noreply.github.com>
2025-05-04 13:35:33 +0300

    fix: make new perk actually spawn

--- server/src/data/lootTables.ts
+++ server/src/data/lootTables.ts
@@ -844,8 +844,8 @@ export const LootTables: Record<ModeName, Record<string, LootTable>> = {
                 { item: PerkIds.CloseQuartersCombat, weight: 1 },
                 { item: PerkIds.LowProfile, weight: 1 },
                 { item: PerkIds.Berserker, weight: 1 },
-                { item: PerkIds.CombatExpert, weight: 1 }
-                // { item: PerkIds.PrecisionRecycling, weight: 1 }
+                { item: PerkIds.CombatExpert, weight: 1 },
+                { item: PerkIds.PrecisionRecycling, weight: 1 }
             ]
         },
         red_gift: [
