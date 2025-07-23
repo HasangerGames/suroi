d9a3510a5088abe3322a15f91a51a063dd45f045
Henry Sanger <henry@sanger.io>
2025-07-23 16:54:19 -0400

    fix: potential issue with map indicator ids

--- server/src/game.ts
+++ server/src/game.ts
@@ -155,8 +155,8 @@ export class Game implements GameData {
      */
     readonly mapPings: PingSerialization[] = [];
 
-    private _nextMapIndicatorID = -1;
-    get nextMapIndicatorID(): number { return ++this._nextMapIndicatorID; }
+    private readonly _mapIndicatorIDAllocator = new IDAllocator(5);
+    get nextMapIndicatorID(): number { return this._mapIndicatorIDAllocator.takeNext(); }
 
     readonly mapIndicators: MapIndicatorSerialization[] = [];
 
@@ -455,6 +455,7 @@ export class Game implements GameData {
 
         for (const indicator of this.mapIndicators) {
             if (indicator.dead) {
+                this._mapIndicatorIDAllocator.give(indicator.id);
                 removeFrom(this.mapIndicators, indicator);
                 continue;
             }
