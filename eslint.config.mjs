d7d08574405ead5dfee0b423d15ef2d131fcdb23
Henry Sanger <henry@sanger.io>
2025-09-06 13:17:44 -0400

    fix: deprecated eslint config method

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,11 @@
 import eslint from "@eslint/js";
 import stylistic from "@stylistic/eslint-plugin";
 import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
 
 import { platform } from "os";
 
-export default tseslint.config(
+export default defineConfig(
     eslint.configs.recommended,
     ...tseslint.configs.strictTypeChecked,
     ...tseslint.configs.stylisticTypeChecked,
